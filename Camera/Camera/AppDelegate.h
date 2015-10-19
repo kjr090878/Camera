6e53a40e4720e33d4f5434b04e7a766de597c2d5
Kelly Robinson <kjr090878@yahoo.com>
2015-10-19 09:25:54 -0400

    Initial create

---
+++ Camera/Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Kelly Robinson on 10/19/15.
+//  Copyright © 2015 Kelly Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
